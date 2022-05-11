b06faa85f67f50be6fe195a29dcebc7275371431
Marcela Magalhaes <marcelamm01@gmail.com>
2022-05-11 22:22:48 +0200

    changing url

--- src/components/PrintCategories.tsx
+++ src/components/PrintCategories.tsx
@@ -18,7 +18,7 @@ export const PrintCategories = ( { categories }: Props ) => {
                         <div className='col-sm-12 col-md-6 col-lg-4' key={ category._id}>
                             <div className='card my-2'>
                                 <Link to={`/products/${category.name}`}>
-                                    <img src={`https://granujillas.teamcamp.ovh/statics/${category.image}`} alt={`Foto de la categoría ${category.name}`} className='category-image card-img-top'/>
+                                    <img src={`https://api.granujillas.teamcamp.ovh/statics/${category.image}`} alt={`Foto de la categoría ${category.name}`} className='category-image card-img-top'/>
                                 </Link>
                                 
                                 <div className='card-body'>
